7e059327d494ba6eea734e254c8a045e8b6a16f8
Matheus Cavalcante <mc2798353@gmail.com>
2024-09-23 12:35:35 -0300

    Feat: alteracao do projeto

--- models/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: {
-        host: 'SEU_BANCO_DE_DADOS',
-        user: 'USER_BANCO_DE_DADOS',
-        password: 'SENHA_BANCO_DE_DADOS',
-        database: 'muninnLibrary'
-    }
-};
