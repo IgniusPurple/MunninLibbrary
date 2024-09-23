3a51a1c0db0a2217e49ed1dc5fb8c26d38740066
Matheus Cavalcante <mc2798353@gmail.com>
2024-09-23 19:30:26 -0300

    Feat: Adicionando alguns usuarios e livros dentro da seeds em rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# Seed de usuários
+User.create(name: 'Alice', email: 'alice@example.com', password: 'password123', password_confirmation: 'password123')
+User.create(name: 'Bob', email: 'bob@example.com', password: 'password123', password_confirmation: 'password123')
+
+# Seed de livros
+Book.create(title: 'O Senhor dos Anéis', author: 'J.R.R. Tolkien', description: 'Uma jornada épica pela Terra Média.')
+Book.create(title: 'Dom Casmurro', author: 'Machado de Assis', description: 'A clássica obra de Machado de Assis sobre ciúmes e desconfiança.')
