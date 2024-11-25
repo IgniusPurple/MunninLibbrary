33e3e0163554343e3971737cdf95b8fdb9d99f6b
João Victor <50639616+joaosantosg@users.noreply.github.com>
2024-11-25 16:32:59 -0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
-    has_secure_password
-  
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
+
+  has_secure_password
+
+  # Validações
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
   end
-  
+end
